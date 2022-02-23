3b8b06576661902a1f8faf319a0951e389bec95a
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-23 23:24:38 +0100

    11. Create the Spring Boot REST Application Creating controllers. closes #45.

---
+++ 11. Create the Spring Boot REST Application/src/main/java/com/academicdeveloper/springauth/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.academicdeveloper.springauth.controller;
+
+/*
+PROJECT NAME : 12. Create the Spring Boot REST Application
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 2/23/2022 11:12 PM
+*/
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/home/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return "Hello " + name + " from " + HomeController.class.getName();
+    }
+}
