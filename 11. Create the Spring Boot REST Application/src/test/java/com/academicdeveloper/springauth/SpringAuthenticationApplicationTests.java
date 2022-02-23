e3fa177ee403f4dc9a5950d88209ad1a9ab39812
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-23 23:19:18 +0100

    11. Create the Spring Boot REST Application Creating the REST Project. closes #44.

---
+++ 11. Create the Spring Boot REST Application/src/test/java/com/academicdeveloper/springauth/SpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicdeveloper.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
